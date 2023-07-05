3f66b6a53ffbb0e1c4d3d45c373f24893d1d6305
Philipova <75500722+Philipova@users.noreply.github.com>
2023-07-05 09:38:02 +0300

    Delete 09. MatchAllWords.js

--- JS-Fundamentals/09. MatchAllWords.js
+++
@@ -1,5 +0,0 @@
-function matchWord(text) {
-    let words = text.match(/\w+/g);
-    console.log(words.join("|"));
-}
-matchWord('A Regular Expression needs to have the global flag in order to match all occurrences in the text');
